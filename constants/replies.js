85c86f94e43310fe8a364375cc3123168e493bdb
Evgeny Rodionov <git@erodionov.ru>
2017-03-29 19:00:41 +0300

    fix: forgot export replies

--- constants/replies.js
+++ constants/replies.js
@@ -1,3 +1,3 @@
-const REPLY_AT_START = `Выберите действие`
-const REPLY_ON_CREATE_JOB = `Теперь выберите категорию`
-const REPLY_ON_CHOOSE_LOCATION = `Регион?`
+export const REPLY_AT_START = `Выберите действие`
+export const REPLY_ON_CREATE_JOB = `Теперь выберите категорию`
+export const REPLY_ON_CHOOSE_LOCATION = `Регион?`
